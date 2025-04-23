34d186a8b119e57d08298b5c48a23fb81c4980b3
kumargeek <130041817+kumargeek@users.noreply.github.com>
2025-04-23 07:47:47 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 412
+let accountEmail = "Kumar.2426mca412@kiet.edu"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2   //not allowed
+// node notices it and gives an error 
+
+console.log(accountId);
+
+
