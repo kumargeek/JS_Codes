babf6b7d977b9791836f7bcf4e11447a90c73db7
kumargeek <130041817+kumargeek@users.noreply.github.com>
2025-04-24 20:57:05 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,7 +6,8 @@ var accountCity = "Jaipur"
 //accountId = 2   //not allowed
 // node notices it and gives an error 
 console.log(accountId);
-console.log(accountCity);
+console.log(accountCity);   //here no error found
+
 
 
 
