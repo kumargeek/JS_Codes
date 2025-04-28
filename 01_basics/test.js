1f60619aeed5051785749e8d1dfa5b1166712825
kumargeek <130041817+kumargeek@users.noreply.github.com>
2025-04-28 05:44:27 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,5 +2,7 @@ console.log("Kumar Kashyap")
 console.log("Here using console.log() to output String");
 console.log(Now again pushing the code); //comment
 console.log("Another day of pushing code in git");  //comment
+console.log("Another code for firemap");
+
 
 
