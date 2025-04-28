6869bdeb91781d209c410c240f53258c251c1b74
kumargeek <130041817+kumargeek@users.noreply.github.com>
2025-04-28 05:43:14 +0000

    contatiner config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,4 +1,6 @@
 console.log("Kumar Kashyap")
 console.log("Here using console.log() to output String");
 console.log(Now again pushing the code); //comment
+console.log("Another day of pushing code in git");
+
 
