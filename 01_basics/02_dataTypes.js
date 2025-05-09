643fc11c608f59879ee50a1af77c94a826e061e8
kumargeek <130041817+kumargeek@users.noreply.github.com>
2025-05-09 10:50:45 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello!") //we are using node.js not browser
+// alert(3+3)
+console.log(3+3)
+console.log("Hello!");
+
+// premetive datatypes
+
+let name = "Kumar"
+let age = 22
+let isLoggedIn = false
+let state
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value/ representation of 
+// undefined -> variable is declered but value is not assigned
+// symbol -> unique
+
+// object
+
+console.log(typeof age);  // gives output number
+console.log(typeof null);   // gives output object
+console.log(typeof undefined);   // gives output  undefined
